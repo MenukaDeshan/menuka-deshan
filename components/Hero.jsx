d76baadbdec6c0483ee27cb554c2bcf25f52546b
Menuka Deshan <64095498+MenukaDeshan@users.noreply.github.com>
2023-09-08 01:22:24 +0530

    Added components About Me

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -13,17 +13,10 @@ export const Hero = () => {
   
   return (
     <section className="flex flex-col-reverse justify-center w-full h-screen md:flex-row md:h-screen" id="Hero">
-      <div className="flex items-center justify-center p-10 h-1/4 md:h-full md:w-2/5">
+      <div className="flex items-center justify-center p-10 h-1/5 md:h-full md:w-2/5">
         <div id="text" className="text-center md:text-left">
         <h1 id='name' className="mb-2 text-3xl font-bold uppercase md:text-5xl hover:text-purple-400">Menuka Deshan</h1>
-          {/* Icons */}
-          <div className='flex justify-center md:justify-start'>
-            <h2 className='flex text-2xl'>
-            <a href='https://github.com/MenukaDeshan' target='blank' className='m-1 hover:text-purple-500'><BsGithub/></a>
-            <a href='https://github.com/MenukaDeshan' target='blank' className='m-1 hover:text-purple-500'><BsFacebook/></a>
-            <a href='https://github.com/MenukaDeshan' target='blank' className='m-1 hover:text-purple-500'><BsInstagram/></a>
-            </h2>
-          </div>
+          {/*
           <p className="text-sm tracking-widest capitalize md:text-md">
             <TypeAnimation
               sequence={[
@@ -38,13 +31,21 @@ export const Hero = () => {
               repeat={Infinity}
               className="py-2 font-light"
             />
-          </p>
-          <p className="text-lg md:text-2xl" id='dev'>Developer and Designer</p>
+          </p>*/}
+          <p className="text-lg md:text-2xl" id='dev'>Web Developer</p>
           <p className="text-sm font-thin md:text-md">
-          I am a passionate and enthusiastic beginner programmer with a strong desire to explore the exciting world of coding. 
-          I am constantly eager to learn and improve my skills, and I enjoy tackling coding challenges and building small projects. 
+          "Bugs are the footprints of the programmer's journey."
           </p>
-          <button className="px-4 py-2 mt-4 text-white bg-purple-500 rounded">Learn More</button>
+          {/* Icons */}
+          <div className='flex justify-center mt-1 md:justify-start'>
+            <h2 className='flex text-2xl'>
+            <a href='https://github.com/MenukaDeshan' target='blank' className='m-1 hover:text-purple-500'><BsGithub/></a>
+            <a href='https://github.com/MenukaDeshan' target='blank' className='m-1 hover:text-purple-500'><BsFacebook/></a>
+            <a href='https://github.com/MenukaDeshan' target='blank' className='m-1 hover:text-purple-500'><BsInstagram/></a>
+            </h2>
+          </div>
+           {/* 3d MODEL 
+          <button className="px-4 py-2 mt-4 text-white bg-purple-500 rounded">Learn More</button>*/}
         </div>
       </div>
 
