84f60fae3f71f16452fec274de9834cc12b17c54
Menuka Deshan <64095498+MenukaDeshan@users.noreply.github.com>
2023-09-09 12:41:11 +0530

    remove about me

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen max-w-[1340px] h-full mx-auto">
       <Hero/>
-      <AboutMe/>
     </main>
   )
 }
